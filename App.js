ed6b43008c9211912d92ce5a525001ac5b541a54
AlfaizT <tambolialfaiz1@gmail.com>
2022-12-11 17:16:43 +0530

    commited js app

---
+++ App.js
@@ -0,0 +1,20 @@
+ console.log('alfaiz')
+var number =10;
+var alpha =30;
+console.log('addition',number+alpha);
+
+var No1 =100;
+var No2 =25;
+console.log('substraction',No1-No2);
+
+var No3 =25;
+var No4=5;
+console.log('dividation',No3/No4);
+
+var No6 =600;
+var No7 =6;
+var No8 =100;
+console.log('calculate percentage',No6/No8*No7);
+
+
+
