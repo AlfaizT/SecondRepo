64d27c69280d5c30614b0949ee52cde009a2be6f
AlfaizT <tambolialfaiz1@gmail.com>
2022-12-11 17:24:21 +0530

    commited program file

---
+++ program.js
@@ -0,0 +1,19 @@
+// console.log("welcome to node js");
+// console.log('Alfaiz');
+// console.log('hello',1);
+// console.log(1000);
+
+let name ="alfaiz"; 
+let company_name = "MNC company";
+var  salary = 100000;
+console.log("welcome",name,"your salary is",salary,company_name);
+
+var number1 = 1000;
+var number2 = 3000;
+console.log("addition is",number1+name);
+
+company_name ="google";
+
+place ="puna";
+
+
